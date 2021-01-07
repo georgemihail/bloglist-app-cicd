a4fc9973e0202b8cf4188c30dbb3afad81f61b10
George Mihail <formy.radusilviu@gmail.com>
2021-01-07 19:00:14 +0200

    Change build

--- bloglist-backend/utils/config.js
+++ bloglist-backend/utils/config.js
@@ -5,6 +5,7 @@ let MONGODB_URI=process.env.MONGODB_URI
 if (process.env.NODE_ENV === 'test') {
     MONGODB_URI=process.env.MONGODB_TEST_URI
 }
+
 const PORT=process.env.PORT || 3001
 
 module.exports = {
